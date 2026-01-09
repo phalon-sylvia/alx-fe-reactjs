bf5d7045ace4e17c4676b2b1ff233bac9739a91d
Phalon <phalonsylvia3@gmail.com>
2026-01-09 09:42:09 +0300

    Import Header, MainContent, and Footer components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -44,3 +44,18 @@ export default App;
 export default App
 
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
