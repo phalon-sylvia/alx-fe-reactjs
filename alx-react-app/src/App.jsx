53af00c4c36d13c532d6bfaf34a8f8283e9f9b4b
Phalon <phalonsylvia3@gmail.com>
2026-01-09 10:24:36 +0300

    Implement UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -73,3 +73,14 @@ function App() {
 }
 
 export default App;
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
